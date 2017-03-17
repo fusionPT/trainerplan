eb9ae494bb7d2b38313ed1b0de54fafb29a38489
Bruno Felicio <bruno@brunofelicio.com>
2017-03-17 12:11:48 +0100

    cleardb config and composer.json changes and db.php changes

--- inc/db.php
+++ inc/db.php
@@ -1,11 +1,18 @@
 <?php
 session_start();
 //$user = json_encode($_SESSION['userna']);
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$servername = $url["host"];
+$username = $url["host"];
+$password = $url["pass"];
+$dbname = substr($url["path"], 1);
 
-$servername = parse_url(getenv("mysql://bc6ecc5d7c3590:4bad3a87@eu-cdbr-west-01.cleardb.com/heroku_7f3212703661806?reconnect=true"));
-$username = "bc6ecc5d7c3590";
-$password = "4bad3a87";
-$dbname = "heroku_7f3212703661806";
+$config = array(
+    'host' => $server ,
+    'user' => $username ,
+    'pw' => $password,
+    'db' => $db
+);
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
