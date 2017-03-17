8d58d01789cb7c8e024403448bf93cd14495c828
Bruno Felicio <bruno@brunofelicio.com>
2017-03-17 11:28:58 +0100

    changed connect db settings again

--- inc/db.php
+++ inc/db.php
@@ -2,7 +2,7 @@
 session_start();
 //$user = json_encode($_SESSION['userna']);
 
-$servername = "mysql://bc6ecc5d7c3590:4bad3a87@eu-cdbr-west-01.cleardb.com/heroku_7f3212703661806";
+$servername = "mysql://bc6ecc5d7c3590:4bad3a87@eu-cdbr-west-01.cleardb.com/heroku_7f3212703661806?reconnect=true";
 $username = "bc6ecc5d7c3590";
 $password = "4bad3a87";
 $dbname = "heroku_7f3212703661806";
